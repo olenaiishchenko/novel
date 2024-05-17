30e31b41f3290a1f9467b7f6027cb187ea1406fe
olenaiishchenko <olena.v.ishchenko@gmail.com>
2024-05-17 12:53:49 +0000

    fixed1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var screen = "MENU";
-var button;
 var img, img1, img2;
 
 function preload() {
@@ -14,50 +13,37 @@ function setup() {
 
 function draw() {
   background(255);
+
   if (screen == "MENU") {
-    background(0)
+    background(0);
     image(img, 50, 0);
 
-    if (mouseIsPressed) {
-      fill("gray");
-      setTimeout(startgame, 1000)
-    } else {
-      fill("white");
-    }
-     rect(300, 500, 100, 50);
-     textSize(20);
+    fill(mouseIsPressed && mouseX > 300 && mouseX < 400 && mouseY > 500 && mouseY < 550 ? "gray" : "white");
+    rect(300, 500, 100, 50);
+    textSize(20);
     fill("black");
-     text("PLAY", 325, 535);
-    
-  } else {
+    text("PLAY", 325, 535);
+
+    if (mouseIsPressed && mouseX > 300 && mouseX < 400 && mouseY > 500 && mouseY < 550) {
+      setTimeout(startgame, 1000);
+    }
+  } else if (screen == "GAME") {
     background(255);
-    image(img1, 1, 1);
     img1.resize(700, 750);
-    image(img2, 300, 70);
+    image(img1, 1, 1);
     img2.resize(500, 1000);
+    image(img2, 300, 70);
 
     fill(255, 255, 255, 170);
     rect(70, 550, 350, 150);
     textSize(25);
     fill("black");
-    text("*Скаржиться на свою роботу*", 75, 580);
+    text("*Скаржитися на свою роботу*", 75, 580);
 
     if (mouseIsPressed) {
-      setTimeout(startgame, 1000)
       img2 = null;
-  } else if (screen == "GAME") {
-      background(255);
-      img1.resize(700, 750);
-      image(img1, 1, 1);
-      img2.resize(500, 1000);
-      image(img2, 300, 70);
-
-      fill(255, 255, 255, 170);
-      rect(70, 550, 350, 150);
-      textSize(25);
-      fill("black");
-      text("*Скаржитися на свою роботу*", 75, 580);
     }
+  }
 }
 
 function startgame() {
